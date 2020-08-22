79fd514cf1dd18041f517a1a8f04f83528df4cff
surya <techbizzsoftwaresolution@gmail.com>
2020-08-22 20:27:06 +0530

    Updates

---
+++ precache-manifest.640d8e1b42a427716c457e8fdb2b6cad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8368376dc1cc289695c085d701e884",
+    "url": "/minireactlive/index.html"
+  },
+  {
+    "revision": "711a59c81cc1e29f8703",
+    "url": "/minireactlive/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7aeb87e3a109b6f67550",
+    "url": "/minireactlive/static/css/main.a55ce196.chunk.css"
+  },
+  {
+    "revision": "711a59c81cc1e29f8703",
+    "url": "/minireactlive/static/js/2.750f4a12.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/minireactlive/static/js/2.750f4a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aeb87e3a109b6f67550",
+    "url": "/minireactlive/static/js/main.c58602e2.chunk.js"
+  },
+  {
+    "revision": "88fd01d75f8d89db2d32",
+    "url": "/minireactlive/static/js/runtime-main.bf7c8703.js"
+  },
+  {
+    "revision": "cdf67f7311006e0f589f8f4a49c75cdf",
+    "url": "/minireactlive/static/media/automated.cdf67f73.png"
+  },
+  {
+    "revision": "378306ce48b60fee075289c3095e4334",
+    "url": "/minireactlive/static/media/baby.378306ce.png"
+  },
+  {
+    "revision": "6a3cabb3a754b362856599627a4c0a57",
+    "url": "/minireactlive/static/media/footer_img.6a3cabb3.png"
+  },
+  {
+    "revision": "a912cf3ef9e45c2ff93e872167036c30",
+    "url": "/minireactlive/static/media/home.a912cf3e.png"
+  },
+  {
+    "revision": "4f0866b9fb9c7760d3b99e2af60bc851",
+    "url": "/minireactlive/static/media/lemon.4f0866b9.png"
+  },
+  {
+    "revision": "fd396c4ceec9344bd53726fa1cf9b94f",
+    "url": "/minireactlive/static/media/logo.fd396c4c.png"
+  },
+  {
+    "revision": "fc2b4a0312cf718670ca88084afeceb0",
+    "url": "/minireactlive/static/media/open.fc2b4a03.png"
+  },
+  {
+    "revision": "6d83339f8bda54febf4eabb94023859e",
+    "url": "/minireactlive/static/media/popularbank.6d83339f.png"
+  }
+]);
